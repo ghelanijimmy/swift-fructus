ece2e79fbedf88f8ec0319586d03e48724ddf9f0
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-24 09:40:26 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Jimmy Ghelani on 2023-09-24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
